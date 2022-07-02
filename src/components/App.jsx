31b62414f2080fb0fb3adffe9b39adddae0abb72
valiantlynx <86688436+valiantlynx@users.noreply.github.com>
2022-07-02 15:03:11 +0100

    live update hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+
+  function handleChange(event) {
+    console.log(
+      event.target.placeholder +
+        " :" +
+        event.target.value +
+        " type:" +
+        event.target.type
+    );
+    setName(event.target.value);
+  }
+
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
+      <h1>Hello {name}</h1>
+      <input
+        onChange={handleChange}
+        type="text"
+        placeholder="What's your name?"
+        value={name}
+      />
       <button>Submit</button>
     </div>
   );
